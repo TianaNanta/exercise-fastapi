d82f95cdc6153bc09fc0629113412220458c0d66
TianaNanta <andrianjakananta@gmail.com>
2023-02-19 16:31:47 +0300

    update admin table

--- exercise/db/models/admin_model.py
+++ exercise/db/models/admin_model.py
@@ -14,5 +14,5 @@ class AdminModel(Base):
     name: Mapped[str] = mapped_column(String(length=200))  # noqa: WPS432
     email: Mapped[str] = mapped_column(String(length=200))  # noqa: WPS432
     hashed_password: Mapped[str] = mapped_column(String(length=300))  # noqa: WPS432
-    avatar: Mapped[bytes] = mapped_column(LargeBinary)  # noqa: WPS432
-    date_creation: Mapped[str] = mapped_column(DateTime(timezone=True), server_default=func.now())  # noqa: WPS432
+    avatar: Mapped[bytes] = mapped_column(LargeBinary(length=300), nullable=True)  # noqa: WPS432
+    date_creation: Mapped[str] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=True)  # noqa: WPS432
